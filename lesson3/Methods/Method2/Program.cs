5fcd91a79c9acb5d9405c0165ff2c0b4284914ad
Shahin Muradli <83555182+Sahin6033@users.noreply.github.com>
2022-05-29 15:56:18 +0400

    Add files via upload

---
+++ lesson3/Methods/Method2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a :");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter b :");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter c :");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double radius;
+            if(TriangleExistenceCondition(a, b, c))
+            {
+                radius =  FindRadius(a, b, c);
+                Console.WriteLine($"Radisu  =\t {radius}");
+                Console.WriteLine($"Area of e circle  =\t {AreaOfCircle(radius)}");
+                Console.WriteLine($"Perimetr of e circle  =\t {PerimetrOfCircle(radius)}");
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("Triangle does not exist");
+            }
+            
+
+        }
+        public static bool TriangleExistenceCondition (double a,double b ,double c)
+        {
+            bool condision = true;
+            if(a+b <= b || a+c <=b || b+c<=a)
+            {
+                condision = false;
+            }
+            return condision;
+        }
+
+        public static double FindRadius(double a, double b, double c)
+        {
+
+            double p = (a + b + c) / 2;
+            double radius = Math.Sqrt(((p-a)*(p-b)*(p-c))/p);
+
+            return radius;
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI*Math.Pow(radius, 2);  
+            return area;
+        }
+
+        public static double PerimetrOfCircle(double radius)
+        {
+            double perimetr =2* Math.PI *radius;
+            return perimetr;
+        }
+    }
+}
---
+++ lesson3/Methods/Methods/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a :");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter b :");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter c :");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            SquareEquality(a,b,c);
+        }
+
+        public static void SquareEquality(double a, double b, double c)
+        {
+            if (a != 0)
+            {
+                double D = b * b - (4 * a * c);
+                if (D < 0)
+                {
+                    Console.WriteLine("The equation has no root");
+                }
+                else if (D == 0)
+                {
+                    Console.WriteLine("x = " + (b * b) / (2 * a));
+                }
+                else
+                {
+                    Console.WriteLine("x1 = " + ((b * b) - Math.Sqrt(D)) / (2 * a));
+                    Console.WriteLine("x2 = " + ((b * b) + Math.Sqrt(D)) / (2 * a));
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"It is a linear equation  : {-c / b}");
+            }
+
+
+
+        }
+    }
+}
+
+
+
+              
+
+       
+
+
