5684086e97ca3d70e0ea078b3ee8aa45414d468e
Shahin Muradli <83555182+Sahin6033@users.noreply.github.com>
2022-07-10 15:01:01 +0400

    Tasks

---
+++ Lesson-12 [07 iyul 2022] tasklar/hashtable.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NonGenericTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable countries = new Hashtable();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Olke adini qeyd edin");
+                string country = Console.ReadLine().ToUpper();
+                Console.WriteLine("Seher adini qeyd edin");
+                string city = Console.ReadLine().ToUpper();
+                countries.Add(country, city);
+            }
+            Console.WriteLine();
+            int keyCode = 13;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Axtarish sozunu yazin");
+                string search = Console.ReadLine();
+                SearchCapitalOfCountry(countries, search);
+                Console.WriteLine("Davam etm'k ucun istenilen duymeni bas;n \n axtarishi dayand;rmaq ucun ESC duymesini basin");
+                ConsoleKey key = Console.ReadKey().Key;
+                keyCode = (int)key;
+                Console.WriteLine();
+            } while (keyCode != 27);
+
+        }
+        public static void SearchCapitalOfCountry(Hashtable ctr, string countryS)
+        {
+            countryS = countryS.ToUpper();
+
+            if (countryS == "ALL")
+            {
+                foreach (DictionaryEntry de in ctr)
+                {
+                    Console.WriteLine("Key: {0}, Value: {1}", de.Key, de.Value);
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("axtarishion neticesi  : "+ctr[countryS]);
+            }
+        }
+
+    }
+}
---
+++ Lesson-12 [07 iyul 2022] tasklar/queue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            int keyCode = 0;
+            
+            
+            while (keyCode != 27)
+            {
+                
+                
+                
+                if(q.Count > 10)
+                {
+                    q.Dequeue();
+                    Console.WriteLine("Reqem daxil edin");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    q.Enqueue(n);   
+                }
+                else
+                {
+                    Console.WriteLine("Reqem daxil edin");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    q.Enqueue(n);
+                }
+
+                Console.WriteLine("Davam etm'k ucun istenilen duymeni bas;n \n axtarishi dayand;rmaq ucun ESC duymesini basin");
+                
+                ConsoleKey key = Console.ReadKey().Key;
+                keyCode = (int)key;
+                Console.WriteLine();
+            }
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
