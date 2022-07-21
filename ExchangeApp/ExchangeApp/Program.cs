5256b6b8e1c510ba9a484c5cbab659ac1a4ae35e
Shahin Muradli <83555182+Sahin6033@users.noreply.github.com>
2022-07-21 18:27:39 +0400

    Add files via upload

---
+++ ExchangeApp/ExchangeApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string req = "";
+            while (req != "n" && req != "N")
+            {
+                Console.WriteLine("Exchange emeliyyatini secin");
+                Console.WriteLine("1: AZN to USD");
+                Console.WriteLine("2: AZN to EUR");
+                Console.WriteLine("3: AZN to TR");
+                Console.WriteLine("4: USD TO AZN");
+                Console.WriteLine("5: EUR TO AZN");
+                Console.WriteLine("6: TR TO AZN");
+                int changeNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Cevirmek istediyiniz  meblegi yazin");
+                double money = Convert.ToDouble(Console.ReadLine());
+                switch (changeNum)
+                {
+                    case 1:
+                        ValutaExchanger.Exchange(CurrencyType.USD, money);
+                        break;
+                    case 2:
+                        ValutaExchanger.Exchange(CurrencyType.EUR, money);
+                        break;
+                    case 3:
+                        ValutaExchanger.Exchange(CurrencyType.TR, money);
+                        break;
+                    case 4:
+                        ValutaExchanger.ExchangeRev(CurrencyType.USD, money);
+                        break;
+                    case 5:
+                        ValutaExchanger.ExchangeRev(CurrencyType.EUR, money);
+                        break;
+                    case 6:
+                        ValutaExchanger.ExchangeRev(CurrencyType.TR, money);
+                        break;
+                    default:
+                        Console.WriteLine("Ele secim movcud deyil");
+                        break;
+
+
+
+                }
+
+                Console.WriteLine("Yeniden Exchange emeliyyati etmek ucun Y duymesini basin \n !!! Eks halda N duymesini basin !!!");
+                req = Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
---
+++ ExchangeApp/ExchangeApp/ValutaExchanger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeApp
+{
+    enum CurrencyType
+    {
+        EUR,
+        USD,
+        TR
+    }
+    internal  class ValutaExchanger
+    {
+
+        public static Dictionary<CurrencyType, double> Curencies = new Dictionary<CurrencyType, double>()
+      {
+          {CurrencyType.USD,1.70},
+          { CurrencyType.TR,0.096},
+          {CurrencyType.EUR,1.73 }
+
+      };
+
+        public static void Exchange(CurrencyType ctype, double money)
+        {
+            double exchanged = money * Curencies[ctype];
+            Console.WriteLine(money+ " AZN to "+ctype +" = "+ exchanged);
+        }
+        public static void ExchangeRev(CurrencyType ctype, double money)
+        {
+            double exchanged = money /Curencies[ctype];
+            Console.WriteLine(money + " "+ctype + "to AZN   = " + exchanged);
+        }
+
+    }
+
+   
+    
+}
