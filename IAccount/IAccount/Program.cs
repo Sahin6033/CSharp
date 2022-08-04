869e93b38e97b0067de27f1e4bb45863fdb5592d
Shahin Muradli <83555182+Sahin6033@users.noreply.github.com>
2022-08-04 15:03:37 +0400

    Add files via upload

---
+++ IAccount/IAccount/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace IAccount
+{
+    interface IAccount
+    {
+
+        bool PasswordChecker();
+        void ShowInfo();
+    }
+
+    class User : IAccount
+    {
+        private string Fullname;
+        private string Email;
+        private string Password;
+        public User(string fn, string email, string ps)
+        {
+            this.Fullname = fn;
+            this.Email = email;
+            this.Password = ps;
+            if (PasswordChecker())
+            {
+                this.Password = ps;
+            }
+            else
+            {
+                this.Password = "";
+                Console.WriteLine("Incorrent type password !!!");
+            }
+
+        }
+      
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Fullname : {Fullname} Password : {Password}");
+        }
+
+        public bool PasswordChecker( )
+        {
+            string ps = this.Password;
+            bool valid = false;
+            bool big = false;
+            bool small = false;
+            bool digit = false;
+            if (ps.Length >= 8)
+            {
+                foreach (char c in ps)
+                {
+                    if (65 <= c && c <= 90)
+                    {
+                        big = true;
+                    }
+                    if (97 <= c && c <= 122)
+                    {
+                        small = true;
+                    }
+                    if (48 <= c && c <= 57)
+                    {
+                        digit = true;
+                    }
+                }
+            }
+            valid = big && small && digit;
+
+            return valid;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Shahin Muradli", "sahin007.sm@gmail.com", "qwSS11udhqwd");
+            user.ShowInfo();
+        }
+    }
+}
---
+++ IAccount/IFigure/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFigure
+{
+    internal class Program
+    {
+
+        interface IFigure
+        {
+            void CalculateArea();
+            void CalculatePerimeter();
+        }
+        class Rectangle : IFigure
+        {
+            double x;
+            double y;
+            public Rectangle(double X, double Y)
+            {
+                this.x = X;
+                this.y = Y;
+            }
+            public void CalculateArea()
+            {
+
+                Console.WriteLine($"Area of Rectangle (a * b) {x} * {y} = {x * y}");
+            }
+
+            public void CalculatePerimeter()
+            {
+                Console.WriteLine($"Perimeter of Rectangle ((a + b) * 2) ({x} + {y})*2 = {(x + y) * 2}");
+            }
+        }
+
+        class Circle : IFigure
+        {
+            double radius;
+            public Circle(double rd)
+            {
+                this.radius = rd;
+            }
+            public void CalculateArea()
+            {
+                Console.WriteLine($"Area of Circle (pi * r ^ 2) {radius} * {radius} * {3.142872} = {radius * radius * 3.142872}");
+            }
+
+            public void CalculatePerimeter()
+            {
+                Console.WriteLine($"Perimeter of Circle (2 * pi * r) {2} * {radius}*{3.142872} = {2 * radius * 3.142872}");
+            }
+        }
+
+        class Triangle : IFigure
+        {
+            double a;
+            double b;
+            double c;
+            public Triangle(double A, double B, double C)
+            {
+                this.a = A;
+                this.b = B;
+                this.c = C;
+            }
+            public void CalculateArea()
+            {
+                double p = (a + b + c) / 2;
+                double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                Console.WriteLine($"Area of Triangle ( p = (a + b + c) / 2 | area = Math.Sqrt(p * (p - a) * (p - b) * (p - c)) ) =   {area}");
+            }
+
+            public void CalculatePerimeter()
+            {
+                Console.WriteLine($"Perimeter of Triangle ( a + b + c)  {a} + {b} + {c} = {a + b + c}");
+            }
+        }
+
+        class Square : IFigure
+        {
+            double a;
+            public Square(double A)
+            {
+                this.a = A;
+            }
+            public void CalculateArea()
+            {
+                Console.WriteLine($" Area o Square (a * a)  {a} * {a} = {a*a}");
+            }
+
+            public void CalculatePerimeter()
+            {
+                Console.WriteLine($"Perimeter of Square (a * 4) {a} * 4 = {a * 4} ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            /*Rectangle rectangle = new Rectangle(5,10);
+            rectangle.CalculateArea();
+            rectangle.CalculatePerimeter();*/
+
+            /* Circle circle = new Circle(7);
+             circle.CalculateArea();
+             circle.CalculatePerimeter();*/
+
+            /*  Triangle triangle = new Triangle(3,4,5);
+              triangle.CalculateArea();
+              triangle.CalculatePerimeter();*/
+
+            Square square = new Square(5);
+            square.CalculateArea();
+            square.CalculatePerimeter();
+        }
+    }
+}
